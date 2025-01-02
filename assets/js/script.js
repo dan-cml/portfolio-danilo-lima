506ebc46976f19dc404e3b1eaee55d9d83b6ee2d
dan-cml <dan_cml@hotmail.com>
2025-01-02 17:30:26 -0300

    Casca final do portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', (e) => {
       e.preventDefault();
       const targetId = link.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      if(targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop - 50,
-          behavior: 'smooth'
-        });
+      if (targetId.startsWith('#')) {
+        const targetElement = document.querySelector(targetId);
+        if(targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - 70, // ajuste para compensar altura do menu
+            behavior: 'smooth'
+          });
+        }
       }
     });
   });
